2424d2c8c2aec8a25f574426e83bec8672abd73d
fmoraless <franticzero@gmail.com>
2019-01-03 23:28:22 -0300

    Seeders

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,12 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        factory(App\User::class,29)->create();
+
+        App\User::create([
+            'name' => 'Francisco Morales',
+            'email' => 'fmorales@mail.com',
+            'password' => bcrypt('123')
+        ]);
     }
 }
