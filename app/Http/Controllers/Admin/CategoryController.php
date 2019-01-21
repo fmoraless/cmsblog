e1621b6c5859f89881b172f20d47a95c48ad8fdd
fmoraless <franticzero@gmail.com>
2019-01-21 19:36:03 -0300

    Métodos de Categorías.

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -4,9 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryStoreRequest;
+use App\Http\Requests\CategoryUpdateRequest;
+
+use App\Category;
 
 class CategoryController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +23,10 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $categories = Category::orderBy('id', 'DESC')->paginate();
+
+        //dd($categories); //para visualizar sin tener la vista creada.
+        return view('admin.categories.index',compact('categories'));
     }
 
     /**
@@ -24,7 +36,7 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.categories.create'); //este método muestra el formulario
     }
 
     /**
@@ -33,9 +45,12 @@ class CategoryController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CategoryStoreRequest $request)
     {
-        //
+        $tag = Category::create($request->all()); //este método salva los datos.
+
+        return redirect()->route('categories.edit', $tag->id)
+            ->with('info', 'Categoría creada con éxito');
     }
 
     /**
@@ -46,7 +61,9 @@ class CategoryController extends Controller
      */
     public function show($id)
     {
-        //
+        $category = Category::find($id); //Ver en detalle una etiqueta
+
+        return view('admin.categories.show', compact('category'));
     }
 
     /**
@@ -57,7 +74,9 @@ class CategoryController extends Controller
      */
     public function edit($id)
     {
-        //
+        $tag = Category::find($id); //muestra la vista para editar
+
+        return view('admin.categories.edit', compact('tag'));
     }
 
     /**
@@ -67,9 +86,14 @@ class CategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(CategoryUpdateRequest $request, $id)
     {
-        //
+        $tag = Category::find($id); //guarda los datos editados
+
+        $tag->fill($request->all())->save();
+
+        return redirect()->route('categories.edit', $tag->id)
+            ->with('info', 'Categoría actualizada con éxito');
     }
 
     /**
@@ -80,6 +104,8 @@ class CategoryController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $tag = Category::find($id)->delete();  //Elimina un registro
+
+        return back()->with('info', 'Eliminado correctamente');
     }
 }
