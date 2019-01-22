b8af7637e9ed98bbc7c8773e6304cadbf28a8187
fmoraless <franticzero@gmail.com>
2019-01-22 12:32:56 -0300

    Metodos de Post controller.

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -4,9 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PostStoreRequest;
+use App\Http\Requests\PostUpdateRequest;
+
+use App\Post;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +23,10 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::orderBy('id', 'DESC')->paginate();
+
+        //dd($posts); //para visualizar sin tener la vista creada.
+        return view('admin.posts.index',compact('posts'));
     }
 
     /**
@@ -24,7 +36,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.posts.create'); //este método muestra el formulario
     }
 
     /**
@@ -33,9 +45,12 @@ class PostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(PostStoreRequest $request)
     {
-        //
+        $tag = Post::create($request->all()); //este método salva los datos.
+
+        return redirect()->route('posts.edit', $tag->id)
+            ->with('info', 'Entrada creada con éxito');
     }
 
     /**
@@ -46,7 +61,9 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        //
+        $post = Post::find($id); //Ver en detalle una etiqueta
+
+        return view('admin.posts.show', compact('post'));
     }
 
     /**
@@ -57,7 +74,9 @@ class PostController extends Controller
      */
     public function edit($id)
     {
-        //
+        $tag = Post::find($id); //muestra la vista para editar
+
+        return view('admin.posts.edit', compact('tag'));
     }
 
     /**
@@ -67,9 +86,14 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(PostUpdateRequest $request, $id)
     {
-        //
+        $tag = Post::find($id); //guarda los datos editados
+
+        $tag->fill($request->all())->save();
+
+        return redirect()->route('posts.edit', $tag->id)
+            ->with('info', 'Entrada actualizada con éxito');
     }
 
     /**
@@ -80,6 +104,8 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $tag = Post::find($id)->delete();  //Elimina un registro
+
+        return back()->with('info', 'Eliminado correctamente');
     }
 }
